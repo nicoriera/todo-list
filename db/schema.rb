76a3c29251ead53b9e0bf1c1f2f06a30f281732d
nicolas riera <nicolariera@gmail.com>
2023-07-18 15:09:51 +0200

    create seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_100532) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_122046) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
